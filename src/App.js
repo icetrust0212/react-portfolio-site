fae45d6ecb68d757a012b2c885d0917912152b53
YC-WebDev2 <yc-webdev2@yinzcam.comm>
2021-08-20 17:08:35 +0300

    neat code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/footer/footer'
+import TestNav from './components/navigation/testnav'
+import { TinyButton as ScrollUpButton } from "react-scroll-up-button";
+
+const App = () => (
+  <>
+    <TestNav></TestNav>
+    <ScrollUpButton></ScrollUpButton>
+    <Footer></Footer>
+  </>
+);
+
+// debugger; // TO INSPECT THE PAGE BEFORE 1ST RENDER
+
+
+export default App;
